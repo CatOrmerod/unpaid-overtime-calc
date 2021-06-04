501729b35bda0796e8869a9b863507cefde9e748
Cat Ormerod <catormerod@gmail.com>
2021-06-04 20:41:17 +1000

    Created Calculation

---
+++ controllers/api/resultsRoute.js
@@ -0,0 +1,19 @@
+const overtimeCalc = require('./utils/calc')
+
+router.get('/results/:email', async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const entryData = await Entry.findByPk(req.params.email, {
+      });
+  
+      const entry = entryData.get({ plain: true });
+      const X, Y, Z
+      overtimeCalc(entry.start, entry.end, entry.lunch, entry.salary);
+      res.render('results', {
+        X, Y, Z,
+      });
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
