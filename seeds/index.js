fa0ed40b9e1fc61159e3fa016199b98b63012fc1
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-06-07 18:23:16 +1000

    more fixes

--- seeds/index.js
+++ seeds/index.js
@@ -1,31 +1,48 @@
+const seedAdmin = require('./admin-seeds.js');
+const seedEntry = require('./entry-seeds.js');
+
 const sequelize = require('../config/connection');
-const { Entry, Admin } = require('../models');
+const {
+    Entry,
+    Admin
+} = require('../models');
 
 // const contributorData = require('./contributorData.json');
 const entryData = require('./entry-seeds');
 const adminData = require('./admin-seeds');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
-
-  await adminData();
-  await entryData();
-
-  /*for (const entry of entryData) {
-    await Entry.create({
-      ...entry,
-      user_id: contributors[Math.floor(Math.random() * contributors.length)].id,
+    await sequelize.sync({
+        force: true
     });
-  }
+    console.log('\n----- DATABASE SYNCED -----\n');
 
-  for (const contributor of contributorData) {
-    await Contributor.create({
-      ...contributor,
-      user_id: contributors[Math.floor(Math.random() * contributors.length)].id,
-    });
-  }*/
+    await seedAdmin();
+    console.log('\n----- ADMIN SEEDED -----\n');
 
-  process.exit(0);
+    await seedEntry();
+    console.log('\n----- ENTRIES SEEDED -----\n');
+
+    //   const admin = await Admin.bulkCreate(adminData, {
+    //     individualHooks: true,
+    //     returning: true,
+    //   });
+
+    //   for (const entry of entryData) {
+    //     await Entry.create({
+    //       ...entry,
+    //       user_id: contributors[Math.floor(Math.random() * contributors.length)].id,
+    //     });
+    //   }
+
+    //   for (const contributor of contributorData) {
+    //     await Contributor.create({
+    //       ...contributor,
+    //       user_id: contributors[Math.floor(Math.random() * contributors.length)].id,
+    //     });
+    //   }
+
+    process.exit(0);
 };
 
 seedDatabase();
