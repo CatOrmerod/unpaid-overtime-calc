ff25c816b8804abe66bb9536fefad3a6ef25f941
Jose <josephgaspar725@gmail.com>
2021-06-08 23:21:06 +1000

    entries-routes: /get-salary endpoint added (salary information)

--- controllers/api/admin-routes.js
+++ controllers/api/admin-routes.js
@@ -26,4 +26,5 @@ router.post('/login', async (req, res) => {
   });
 });
 
+
 module.exports = router;
--- controllers/api/entries-routes.js
+++ controllers/api/entries-routes.js
@@ -44,17 +44,70 @@ router.get('/get-entries-total', async (req, res) => {
   const entries = await Entry.findAll();
 
   const total = entries.reduce((total, { start_time, end_time, salary }) => {
-      if (start_time > end_time) {
-          start_time = start_time - 24;
-      }
+    if (start_time > end_time) {
+      start_time = start_time - 24;
+    }
 
-      const workedHours = end_time - start_time;
-      if (0 > workedHours) console.log('THERE IS AN ERROR', end_time, start_time)
+    const workedHours = end_time - start_time;
+    // if (0 > workedHours) console.log('THERE IS AN ERROR', end_time, start_time)
 
-      return total + salary * workedHours;
+    return total + salary * workedHours;
   }, 0);
 
   res.json({ total });
+});
+
+router.get('/get-salary', async (req, res) => {
+  const payment = 7;
+  const extraPayment = 10;
+
+  const entries = (await Entry.findAll()).map((entry) => {
+    let {
+      id,
+      industry,
+      had_lunch,
+      name,
+      email,
+      created_at,
+      start_time,
+      end_time,
+      salary,
+    } = entry;
+
+    const offset = start_time > end_time ? 24 : 0;
+
+    const workedHours = end_time - (start_time - offset);
+
+    if (0 > workedHours) console.log('THERE IS AN ERROR', end_time, start_time);
+    
+    const regularHours = Math.min(8, workedHours);
+    const extraHours = workedHours - regularHours;
+
+    const regular_salary = regularHours * payment;
+    const extra_salary = extraHours * extraPayment
+    const daily_salary = regular_salary + extra_salary;
+
+    console.log(daily_salary);
+
+    return {
+      id,
+      industry,
+      had_lunch,
+      name,
+      email,
+      created_at,
+      start_time,
+      end_time,
+      regularHours,
+      extraHours,
+      salary,
+      regular_salary,
+      extra_salary,
+      daily_salary,
+    }
+  });
+
+  res.json({ entries });
 });
 
 module.exports = router;
